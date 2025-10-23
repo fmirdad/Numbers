3873a042cb2b2f4236f121893ddc85e12c541a9a
Frank Mirdad <fmirdad1@gmail.com>
2025-10-23 17:01:04 +0100

    Updated function to calculate the power of 2 numbers provided through user input.

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@
             input = Console.ReadLine();
             num2 = Convert.ToInt32(input);
             Console.WriteLine("Sum is : {0}", sum(num1, num2));
+            Console.WriteLine("Power is : {0}", power(num1, num2));
             //{0} takes 1st argument and substitutes into string
             Console.ReadKey();  //waits for a key to press before ending
 
@@ -21,6 +22,18 @@
         static int sum(int x, int y)
         {
             return x + y;
+        }
+
+        static int power(int x, int y)
+        {
+            int z = x;
+            for (int i = 0; i < y; i++) // for loop where i = 0, run loop whilst i is less than y, add 1 to i each time loop runs.
+            {
+
+               z = z * x;  // each time the loop runs, multiply z by x and set z to the result.
+            }
+            return z;
+
         }
 
     }
